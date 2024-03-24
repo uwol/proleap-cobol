662fb743059fe4b1ee5ed91bbe2c0fe7b6a43717
Ulrich Wolffgang <uwol@users.noreply.github.com>
2024-03-24 13:53:17 +0100

    removed UI

--- ui/www.proleap.io/src/params.js
+++
@@ -1,2 +0,0 @@
-export const APP_URL = process.env.VUE_APP_APP_URL
-export const LOCALE_FALLBACK = 'en'
--- ui/www.proleap.io/src/urls.js
+++
@@ -1,6 +0,0 @@
-import { APP_URL } from '@/params'
-
-export const ANALYZE_URL = `${APP_URL}/v1/cobol/analyze/text`
-export const EXECUTE_URL = `${APP_URL}/v1/cobol/execute/text`
-export const MAIL_URL = `${APP_URL}/web/mail`
-export const TRANSFORM_URL = `${APP_URL}/v1/cobol/transform/text`
